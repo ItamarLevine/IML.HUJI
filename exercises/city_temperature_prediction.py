8703a5566aef88ee2f6b8e2e0d9810b9323ee6d5
itamar.levine <mishlev307>
2022-03-31 18:06:04 +0300

    q1, q2, q3 done

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -1,12 +1,10 @@
 import IMLearn.learners.regressors.linear_regression
 from IMLearn.learners.regressors import PolynomialFitting
 from IMLearn.utils import split_train_test
-
+from datetime import datetime
 import numpy as np
 import pandas as pd
-import plotly.express as px
-import plotly.io as pio
-pio.templates.default = "simple_white"
+import matplotlib.pyplot as plt
 
 
 def load_data(filename: str) -> pd.DataFrame:
@@ -21,20 +19,58 @@ def load_data(filename: str) -> pd.DataFrame:
     -------
     Design matrix and response vector (Temp)
     """
-    raise NotImplementedError()
+    full_data = pd.read_csv(filename, parse_dates=True).dropna().drop_duplicates()
+    # feature i dropped: "lat","long", "id", "date",waterfront
+    full_data = full_data.drop(np.where(full_data["Temp"].values < -10)[0])
+    features = full_data[["Country", "City", "Date", "Year", "Month", "Day", "Temp"]]
+    vec_convert_date = np.vectorize(convert_string_date_to_num)
+    features = features.assign(day_of_year=vec_convert_date(features["Date"]))
+    return features
+
+
+def convert_string_date_to_num(string_date):
+    dt = datetime.strptime(string_date, '%Y-%m-%d')
+    return dt.timetuple().tm_yday
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
-    raise NotImplementedError()
+    df = load_data("../datasets/City_Temperature.csv")
 
     # Question 2 - Exploring data for specific country
-    raise NotImplementedError()
+    sub_df = df.loc[df['Country'] == "Israel"]
+    graph = plt.figure()
+    plt.title('Temp as function of day of year')
+    plt.ylabel('Temp')
+    plt.xlabel("day of year")
+    for year in np.unique(sub_df["Year"]):
+        ind = np.where(sub_df["Year"] == year)[0].astype(int)
+        temp = sub_df["Temp"].values[ind]
+        days = sub_df["day_of_year"].values[ind]
+        plt.scatter(days, temp, label=str(year),s=2)
+    plt.legend(loc='right')
+    plt.show()
 
+    by_month = sub_df.groupby('Month').agg('std')
+    graph = plt.figure()
+    plt.title('std of temp as function of month')
+    plt.ylabel('Temp')
+    plt.xlabel("month")
+    plt.bar(np.arange(12)+1,by_month["Temp"])
+    plt.show()
     # Question 3 - Exploring differences between countries
-    raise NotImplementedError()
-
+    graph = plt.figure()
+    plt.title('std of temp as function of month')
+    plt.ylabel('Temp')
+    plt.xlabel("month")
+    for county in np.unique(df["Country"]):
+        sub_df = df.loc[df['Country'] == county]
+        by_country = sub_df.groupby("Month").Temp.agg(['mean', 'std'])
+        plt.plot(np.arange(12)+1,by_country.values[:,0], label=county)
+        plt.fill_between(np.arange(12)+1, by_country.values[:,0] - by_country.values[:,1], by_country.values[:,0] + by_country.values[:,1], alpha=0.2)
+    plt.legend(loc='upper left')
+    plt.show()
     # Question 4 - Fitting model for different values of `k`
     raise NotImplementedError()
 
