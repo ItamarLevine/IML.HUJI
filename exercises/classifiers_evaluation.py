563081148a63eca5f71e92f51c95f758fb5a1d57
ItamarLevine <itamar307@gmail.com>
2022-04-19 23:31:23 +0300

    almost finish, rename to add lables for plots

--- exercises/classifiers_evaluation.py
+++ exercises/classifiers_evaluation.py
@@ -1,12 +1,10 @@
 import numpy as np
-
 from IMLearn.learners.classifiers import Perceptron, LDA, GaussianNaiveBayes
 from typing import Tuple
 from utils import *
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from math import atan2, pi
 import matplotlib.pyplot as plt
+from IMLearn.metrics import loss_functions
+from math import atan2
 
 
 def load_dataset(filename: str) -> Tuple[np.ndarray, np.ndarray]:
@@ -56,7 +54,7 @@ def run_perceptron():
         plt.show()
 
 
-def get_ellipse(mu: np.ndarray, cov: np.ndarray):
+def get_ellipse(mu: np.ndarray, cov: np.ndarray, fig):
     """
     Draw an ellipse centered at given location and according to specified covariance matrix
 
@@ -74,11 +72,11 @@ def get_ellipse(mu: np.ndarray, cov: np.ndarray):
     """
     l1, l2 = tuple(np.linalg.eigvalsh(cov)[::-1])
     theta = atan2(l1 - cov[0, 0], cov[0, 1]) if cov[0, 1] != 0 else (np.pi / 2 if cov[0, 0] < cov[1, 1] else 0)
-    t = np.linspace(0, 2 * pi, 100)
+    t = np.linspace(0, 2 * np.pi, 100)
     xs = (l1 * np.cos(theta) * np.cos(t)) - (l2 * np.sin(theta) * np.sin(t))
     ys = (l1 * np.sin(theta) * np.cos(t)) + (l2 * np.cos(theta) * np.sin(t))
-
-    return go.Scatter(x=mu[0] + xs, y=mu[1] + ys, mode="lines", marker_color="black")
+    fig.scatter(mu[0], mu[1], marker='x',color='black',s=20)
+    return fig.scatter(x=mu[0] + xs, y=mu[1] + ys,s=3)
 
 
 def compare_gaussian_classifiers():
@@ -91,30 +89,48 @@ def compare_gaussian_classifiers():
 
         # Fit models and predict over training set
         lda = LDA().fit(X, y)
-        pred = lda.predict(X)
-        graph = plt.figure()
-        plt.title(f"perceptron's loss a")
-        plt.ylabel('loss')
-        plt.xlabel("iterations")
-        plt.scatter(np.arange(len(y)), y,label="True class",s=4, color='red')
-        plt.scatter(np.arange(len(y)), pred,label="Predicted class",s=4,color='green')
-        plt.ylim(-0.5,3)
-        plt.legend(loc='best')
-        plt.show()
+        gnb = GaussianNaiveBayes().fit(X, y)
         # Plot a figure with two suplots, showing the Gaussian Naive Bayes predictions on the left and LDA predictions
         # on the right. Plot title should specify dataset used and subplot titles should specify algorithm and accuracy
         # Create subplots
-        from IMLearn.metrics import accuracy
-        raise NotImplementedError()
-
-        # Add traces for data-points setting symbols and colors
-        raise NotImplementedError()
-
-        # Add `X` dots specifying fitted Gaussians' means
-        raise NotImplementedError()
-
-        # Add ellipses depicting the covariances of the fitted Gaussians
-        raise NotImplementedError()
+        fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True)
+        fig.suptitle(f"compare LDA and GBA prediction on dataset: {f}")
+        fig.supxlabel("X axis")
+        fig.supylabel("Y axis")
+        fig.set_figwidth(12)
+        for i,model,name in ((0,lda, "LDA"),(1,gnb,"GBA")):
+            pred = model.predict(X)
+            acc = loss_functions.accuracy(y, pred)
+            ax[i].title.set_text(f"{name} accuracy is {acc}")
+            # Add traces for data-points setting symbols and colors
+            for c in np.unique(y):
+                for d in np.unique(y):
+                    if c == 0:
+                        shape = "."
+                    elif c == 1:
+                        shape = "^"
+                    else:
+                        shape = "s"
+                    if d == 0:
+                        color = "blue"
+                    elif d == 1:
+                        color = "red"
+                    else:
+                        color = "green"
+                    ind_true = np.where(y == c)[0]
+                    ind_pred = np.where(pred == d)[0]
+                    ind = np.intersect1d(ind_pred,ind_true)
+                    samples = X[ind]
+                    ax[i].scatter(samples[:,0], samples[:,1], marker=shape, color=color)
+                # Add `X` dots specifying fitted Gaussians' means
+                # Add ellipses depicting the covariances of the fitted Gaussians
+                if model == lda:
+                    cov = model.cov_
+                else:
+                    cov = np.linalg.inv(np.diag(model.vars_[c].reshape(model.vars_.shape[1], )))
+                get_ellipse(model.mu_[c], cov, ax[i])
+        fig.legend(loc='lower left')
+        plt.show()
 
 
 if __name__ == '__main__':
